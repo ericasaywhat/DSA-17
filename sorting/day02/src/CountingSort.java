e048d21ee0340a09a87765015d9968921c42de59
Erica Lee <erica.lee@students.olin.edu>
2017-03-04 11:33:13 -0500

    ADD: Completed Day2 of sorting

--- sorting/day02/src/CountingSort.java
+++ sorting/day02/src/CountingSort.java
@@ -1,11 +1,25 @@
+import static sun.swing.MenuItemLayoutHelper.max;
+
 public class CountingSort {
 
     /**
      * Use counting sort to sort positive integer array A.
-     * Runtime: TODO
+     * Runtime: O(n+k)
      */
     static void countingSort(int[] A) {
-        // TODO
+        int k = max(A);
+        int[] counts = new int[k+1];
+        for (int i: A){
+            counts[i] ++;
+            }
+        int a = 0;
+        for (int j = 0; j<=k; j++){      //runtime k
+            while(counts[j] > 0){       //runtime n; independent of each other
+                A[a] = j;
+                a++;
+                counts[j]--;
+            }
+        }
     }
 
 }
--- sorting/day02/src/Problems.java
+++ sorting/day02/src/Problems.java
@@ -1,7 +1,16 @@
+import java.util.LinkedList;
+
 public class Problems {
 
     static void sortNumsBetween100s(int[] A) {
-        // TODO
+        RadixSort rs = new RadixSort();
+        for (int i = 0; i < A.length ; i++) {
+            A[i]+=100;
+        }
+        rs.radixSort(A,10);
+        for (int i = 0; i < A.length ; i++) {
+            A[i]-=100;
+        }
     }
 
     /**
@@ -19,14 +28,29 @@ public class Problems {
      * @param n The digit number (where 0 is the least significant digit)
      */
     static void countingSortByCharacter(String[] A, int n) {
-        // TODO
+        LinkedList<String>[] L = new LinkedList[26];
+        for (int i = 0; i < 26; i++)
+            L[i] = new LinkedList<>();
+        for (String el : A) {
+            int nth = getNthCharacter(el,n);
+            L[nth].add(el);
+        }
+        int j = 0; // index in A to place numbers
+        for (LinkedList<String> list : L) {
+            if (!list.isEmpty()) {
+                for (String el2 : list) A[j++] = el2;
+            }
+        }
+
     }
 
     /**
      * @param stringLength The length of each of the strings in S
      */
     static void sortStrings(String[] S, int stringLength) {
-        // TODO
+        for (int i = 0; i < stringLength; i++) {
+            countingSortByCharacter(S, i);
+        }
     }
 
 }
