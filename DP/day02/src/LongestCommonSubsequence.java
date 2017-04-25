bc07985604da59742ece2f2bb456d549674ab947
Erica Lee <erica.lee@students.olin.edu>
2017-04-25 16:09:42 -0400

    ADD: DP Day2

--- DP/day02/src/LongestCommonSubsequence.java
+++ DP/day02/src/LongestCommonSubsequence.java
@@ -1,8 +1,16 @@
 public class LongestCommonSubsequence {
 
     public static int longestCommonSubsequence(String S, String T) {
-        // TODO
-        return -1;
-    }
+        int [][] memo = new int [S.length()+1][T.length()+1];
 
+        for (int k = 0; k<=S.length(); k++) {
+            for (int l = 0; l<=T.length(); l++){
+                if (k == 0 || l == 0) memo[k][l] = 0;
+                else if(S.charAt(k-1) == T.charAt(l-1)) memo[k][l] = memo[k-1][l-1] + 1;
+                else memo[k][l] = Math.max(memo[k-1][l], memo[k][l-1]);
+            }
+        }
+
+        return memo[S.length()][T.length()];
+    }
 }
--- DP/day02/src/Trampolines.java
+++ DP/day02/src/Trampolines.java
@@ -1,6 +1,42 @@
 public class Trampolines {
     public static int trampoline(int[] nums) {
-        // TODO
-        return -1;
+        int[] memo = new int [nums.length];
+        for (int i = 0; i <nums.length; i++) {
+            memo[i] =-1;
+        }
+        memo[nums.length-1] = 0;
+
+
+        for (int i = nums.length-2; i >= 0 ; i--) {
+            trampolineHelper(nums, memo, i);
+        }
+
+        return memo[0];
+    }
+
+
+    private static int trampolineHelper(int[] nums, int[] memo, int i){
+        if(memo[i] != -1) return memo[i];
+
+        int currentMin = nums.length;
+        int nextTramp = i + nums[i];
+        if (nextTramp >= nums.length){
+            memo[i] = 1;
+            return memo[i];
+        }
+
+        for (int j = i+1; j <=nextTramp; j++) {
+            int cost = memo[j];
+            if(cost<currentMin){
+                currentMin = cost;
+            }
+        }
+        memo[i] = currentMin+1;
+        return currentMin;
+    }
+
+    public static void main(String[] args){
+        int[] w = {1,3,1,1,1};
+        System.out.println(trampoline(w));
     }
 }
